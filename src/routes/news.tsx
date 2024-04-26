ae2483b85a553b31bd1b842dd8c17f9f895299d5
Jeong Byeong Ho <zero8660@gmail.com>
2024-04-26 20:58:53 +0900

    input 태그에 onChange 이벤트 기능 구현.

--- src/routes/news.tsx
+++ src/routes/news.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 export default function News(): JSX.Element {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div>
-      <div></div>
+      <form>
+        <input onChange={onChange} value={inputValue} />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
