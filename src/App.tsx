2371ca9d695223532f9e8f4f39253601950177d5
Jeong Byeong Ho <zero8660@gmail.com>
2024-04-26 20:29:38 +0900

    font 적용, logo, nav-bar UI 임시 구현.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import "../src/styles/App.module.css";
+import styles from "../src/styles/App.module.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div>
-      <div></div>
+      <h1 className={styles.logo}>Custom Board</h1>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>News</li>
+          <li>Schedule</li>
+          <li>Something</li>
+          <li>Something</li>
+          <li>Favorites</li>
+        </ul>
+      </nav>
     </div>
   );
 }
