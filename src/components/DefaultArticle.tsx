0fa8051baac1e3359336e5cb56e538365cd14c26
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-08 00:00:46 +0900

    뉴스 기사 초기값 상태일 때, 기사 리스트 컨테이너 UI 추가.

--- src/components/DefaultArticle.tsx
+++ src/components/DefaultArticle.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/DefaultArticle.module.css";
 export default function DefaultArticle(): JSX.Element {
   return (
     <div className={styles.articleContainer}>
-      <div>This is replacement.</div>
+      <div className={styles.text}>Search for your interests.</div>
     </div>
   );
 }
