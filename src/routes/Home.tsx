b88c06f68c56e278ec2810ae2f1589181ad846fe
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-16 17:49:33 +0900

    이미지 셀렉터 개발 취소. (이미지의 다양성 부족)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,8 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h1>This is Home</h1>
+    <div className={styles.container}>
       <p>브라우저의 광고 차단 익스텐션을 사용중이라면 컨텐츠가 제대로 표시되지 않을 수 있습니다.</p>
     </div>
   );
--- src/routes/News.tsx
+++ src/routes/News.tsx
@@ -1,19 +1,14 @@
 import styles from "../styles/News.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../store/store";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 import ArticleContainer from "../components/ArticleContainer";
 import { useEffect, useState } from "react";
 import NewsPreview from "../components/NewsPrivew";
-import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
-import { setImgIndex } from "../store/imgIndexSlice";
 
 export default function News(): JSX.Element {
   const newsData = useSelector((state: RootState) => state.newsData);
   const current = useSelector((state: RootState) => state.visibility);
-  const currentNews = useSelector((state: RootState) => state.currentNews);
-  const imgIndex = useSelector((state: RootState) => state.imgIndex);
   const [borderToggle, setBorderToggle] = useState<boolean>(true);
-  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     if (newsData.length !== 0) {
@@ -23,31 +18,11 @@ export default function News(): JSX.Element {
     }
   }, [newsData]);
 
-  const onLeftClick = () => {
-    if (imgIndex === 0) {
-      return;
-    } else if (imgIndex < currentNews.imageUrls.length) {
-      dispatch(setImgIndex(imgIndex - 1));
-    }
-  };
-
-  const onRightClick = () => {
-    imgIndex < currentNews.imageUrls.length - 1 ? dispatch(setImgIndex(imgIndex + 1)) : null;
-  };
-
   return (
     <div>
       <div className={styles.newsContainer}>
         <div className={styles.previewContainer}>
-          <div className={styles.topBar}>
-            <div className={styles.previewTitle}>Preview</div>
-            {currentNews.title !== "" ? (
-              <div className={styles.imgSeletor}>
-                <IoMdArrowDropleft className={`${styles.leftBtn} ${styles.btn}`} onClick={onLeftClick} />
-                <IoMdArrowDropright className={`${styles.rightBtn} ${styles.btn}`} onClick={onRightClick} />
-              </div>
-            ) : null}
-          </div>
+          <div className={styles.previewTitle}>Preview</div>
           {newsData.length !== 0 ? (
             newsData.slice(current, current + 1).map((item) => <NewsPreview article={item} key={current} />)
           ) : (
