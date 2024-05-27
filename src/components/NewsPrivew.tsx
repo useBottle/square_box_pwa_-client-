a21113732cb5bbe484fd5eb795701829ef0f9281
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-28 01:21:56 +0900

    만약 뉴스 기사 텍스트가 없다면 텍스트 정제 함수가 동작하지 않고 대체 텍스트를 보여주도록 설정.

--- src/components/NewsPrivew.tsx
+++ src/components/NewsPrivew.tsx
@@ -17,26 +17,32 @@ export default function NewsPreview({ article }: NewsProps): JSX.Element {
     ? article.imageUrls[0]
     : process.env.REACT_APP_DEFAULT_NEWS_IMAGE;
 
-  function cleanText(text: string) {
-    // 빈 줄을 기준으로 텍스트를 분할
-    let sections = text.split(/\n\s*\n/);
+  function cleanText(text: string): string | void {
+    try {
+      // 빈 줄을 기준으로 텍스트를 분할
+      let sections = text.split(/\n\s*\n/);
 
-    // 20자 이하의 섹션 제거
-    sections = sections.filter((section) => section.trim().length > 20);
+      // 20자 이하의 섹션 제거
+      sections = sections.filter((section) => section.trim().length > 20);
 
-    // 모든 섹션을 하나의 문자열로 병합하고, 두 줄 이상의 빈 줄을 하나로 통일
-    const pressedText = sections.join("\n\n").replace(/\n{3,}/g, "\n\n");
+      // 모든 섹션을 하나의 문자열로 병합하고, 두 줄 이상의 빈 줄을 하나로 통일
+      const pressedText = sections.join("\n\n").replace(/\n{3,}/g, "\n\n");
 
-    // "다." 문자를 기준으로 줄 바꿈 삽입하여 문단 나누기.
-    const addEmptyLine = pressedText.replace(/다\.\s*(?=\S)/g, "다.\n\n");
+      // "다." 문자를 기준으로 줄 바꿈 삽입하여 문단 나누기.
+      const addEmptyLine = pressedText.replace(/다\.\s*(?=\S)/g, "다.\n\n");
 
-    // 텍스트 전문의 맨 앞과 뒤의 공백 모두 제거.
-    const removeSpaces = addEmptyLine.trim();
+      // 텍스트 전문의 맨 앞과 뒤의 공백 모두 제거.
+      const removeSpaces = addEmptyLine.trim();
 
-    return removeSpaces;
+      return removeSpaces;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  const editedText = cleanText(article.articleText);
+  const editedText = article.articleText
+    ? cleanText(article.articleText)
+    : (process.env.REACT_APP_NO_ARTICLE_MESSAGE as string);
 
   // a 태그 대신 사용. 브라우저 하단에 URL 미리보기 나타나는 것 방지하기 위한 용도.
   const openNewTab = (url: string): void => {
@@ -52,7 +58,7 @@ export default function NewsPreview({ article }: NewsProps): JSX.Element {
         <button className={styles.originalLink} onClick={() => openNewTab(article.originallink)}>
           원문 링크
         </button>
-        <p>{editedText ? editedText : process.env.REACT_APP_NO_ARTICLE_MESSAGE}</p>
+        <p>{editedText as string}</p>
       </div>
     </div>
   );
