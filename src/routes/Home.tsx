d2c325b02a35e65b3f772b92bf9b02179582e528
Jeong Byeong Ho <zero8660@gmail.com>
2024-05-29 01:01:32 +0900

    실검 업데이트 남은시간 카운터 기능 구현을 위해 시간 계산 로직 구현.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,6 +11,9 @@ export default function Home(): JSX.Element {
   const dispatch = useDispatch();
   const realTimeSearchTerms = useSelector((state: RootState) => state.realTimeSearchTerm);
   const darkLightToggle = useSelector((state: RootState) => state.darkLight);
+  const [loadedTime, setLoadedTime] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+  const [gauge, setGauge] = useState<number>(0);
 
   const fetchKeyword = async (): Promise<void> => {
     try {
@@ -24,14 +27,37 @@ export default function Home(): JSX.Element {
 
   useEffect(() => {
     fetchKeyword();
+
+    // 페이지 최초 로드 한 순간의 시간 저장.
+    const minutes = new Date().getMinutes();
+    const seconds = new Date().getSeconds();
+
+    // 로드 시간을 10분 = 600초 구간안에서 몇 초인지  계산.
+    setLoadedTime((minutes % 10) * 60 + seconds);
+
+    // 페이지 첫 로드 이후 초 단위 시간 카운트.
+    const intervalId = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+      setCount(0);
+      setGauge(0);
+    };
   }, []);
+
+  useEffect(() => {
+    // 10분 단위 안에서 현 시간 기준으로 지난 시간을 백분율로 소수점 두 자리수 까지 계산.
+    setGauge(Number(((loadedTime + count) / 6).toFixed(2)));
+  }, [count]);
 
   useEffect(() => {
     // 5초마다 분단위로만 현재 시간 저장.
     const intervalId = setInterval(() => {
-      setCurrentMinutes(new Date().getMinutes());
-      console.log(currentMinutes);
-    }, 5000);
+      const minutes = new Date().getMinutes();
+      setCurrentMinutes(minutes);
+    }, 1000);
 
     // 매 시간 minute 가 10의 배수일 때만 실시간 검색어 요청.
     if (currentMinutes % 10 === 0) {
@@ -47,8 +73,6 @@ export default function Home(): JSX.Element {
   useEffect(() => {
     console.log(realTimeSearchTerms);
   }, [realTimeSearchTerms]);
-
-  console.log(realTimeSearchTerms.top10);
 
   return (
     <section data-theme={darkLightToggle === "dark" ? "" : "light"}>
