a3fa4bd3c713b9d85c339d6672128a59d2195909
Jeong Byeong Ho <zero8660@gmail.com>
2024-06-11 13:41:13 +0900

    홈 화면 구성.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,6 +8,7 @@ import { setInputValue } from "../store/inputValueSlice";
 import { setNewsData, setRealTimeSearchTerms, setYoutubeData } from "../store/dataSlice";
 import { setNewsLoading, setSearchModalTrigger, setYoutubeLoading } from "../store/userInterfaceSlice";
 import { MESSAGE } from "../common/message";
+import { FaInfoCircle } from "react-icons/fa";
 
 export default function Home(): JSX.Element {
   const dispatch = useDispatch();
@@ -97,6 +98,31 @@ export default function Home(): JSX.Element {
 
   return (
     <section className={styles.HomeContainer}>
+      <div className={styles.landingContainer}>
+        <FaInfoCircle className={styles.infoIcon} />
+        <div className={styles.iconBack} />
+        <div className={styles.infoContainer}>
+          <p>
+            <p>
+              <span>Square Box</span> 에 오신 것을 환영합니다. <br />
+            </p>
+            <p>
+              검색어로 검색하면 관련 뉴스 기사 및 유튜브 영상을 <br />
+              보실 수 있습니다. <br />
+            </p>
+            <p>
+              이 서비스는 웹 스크래핑 기술로 컨텐츠를 제공합니다. <br />
+              서버에서 데이터를 수집하는 시간으로 인해 로딩 시간이 <br />
+              길어질 수 있습니다.
+            </p>
+            <p>뉴스 기사는 언론사 측의 보안 설정에 따라 원활히 제공되지 않는 경우도 있을 수 있습니다.</p>
+            <p>
+              실시간 검색어의 업데이트 주기는 약 10분이며, <br />
+              브라우저 환경을 고려해 1분마다 업데이트 여부를 갱신하고 있습니다.
+            </p>
+          </p>
+        </div>
+      </div>
       <div className={styles.realTimeContainer}>
         <h4 className={styles.realTimeTitle}>실시간 검색어 Top 10</h4>
         <div className={styles.updateCounter}>
