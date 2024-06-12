aa6a5cf46a91d2fde577ccec885fa0834e55c50f
Jeong Byeong Ho <zero8660@gmail.com>
2024-06-12 23:38:30 +0900

    회원가입 페이지 UI 구현.

--- src/common/message.ts
+++ src/common/message.ts
@@ -11,4 +11,13 @@ export const MESSAGE = {
   ERROR: {
     NO_ARTICLE: "기술적인 문제로 해당 언론사에서 데이터를 가져올 수 없습니다.\n원문 링크를 방문해주세요. ",
   },
+
+  SIGNUP: {
+    EMAIL_INFO: "이메일 주소를 입력해주세요. (@ 기호 및 . 기호 이후 문자 2개 이상)",
+    EMAIL_WRONG: "이메일 형식이 맞지 않습니다.",
+    PASSWORD_INFO: "사용할 비밀번호를 입력해주세요. (6글자 이상, 특수문자 1개 이상)",
+    PASSWORD_WRONG: "비밀번호 형식이 맞지 않습니다.",
+    PASSWORD_CONFIRM: "비밀번호를 한 번 더 입력해주세요.",
+    CONFIRM_WRONG: "입력한 비밀번호와 다릅니다.",
+  },
 };
