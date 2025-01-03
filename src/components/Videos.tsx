519edb7bf02b4de7314b0a9bbca89f986c29a54d
Jeong Byeong Ho <zero8660@gmail.com>
2025-01-03 18:35:21 +0900

    youtubeData 의 각 요소에 적용되는 onMouseEnter 함수를 map 밖으로 위치.

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -2,27 +2,22 @@ import { AppDispatch, RootState } from "../store/store";
 import styles from "../styles/Videos.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentYoutube } from "../store/youtubeSlice";
+import { YouTubeVideo } from "../types/types";
 
 export default function Videos(): JSX.Element {
   const dispatch = useDispatch<AppDispatch>();
   const { youtubeData } = useSelector((state: RootState) => state.data);
 
+  const onMouseEnter = (item: YouTubeVideo): void => {
+    dispatch(setCurrentYoutube(item));
+  };
+
   return (
     <div className={styles.videoSlider}>
       {youtubeData.items.map((item, index) => {
-        const onMouseEnter = (): void => {
-          dispatch(setCurrentYoutube(item));
-        };
-
         return (
-          <div
-            key={index}
-            className={styles.videoList}
-            onMouseEnter={() => {
-              dispatch(setCurrentYoutube(item));
-            }}
-          >
-            <div onMouseEnter={onMouseEnter} className={styles.video}>
+          <div key={index} className={styles.videoList}>
+            <div onMouseEnter={() => onMouseEnter(item)} className={styles.video}>
               <img src={item.snippet.thumbnails.high.url} alt="thumbnail" />
               <div className={styles.videoText}>
                 <h3>{item.snippet.title}</h3>
